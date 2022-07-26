e01a97766ab8a403e69a114c8d8e6810c15be2d5
0x7FFFFF <w13657804853@163.com>
2022-07-27 02:27:31 +0800

    Complete global force simplified function

--- src/blueprints/history.py
+++ src/blueprints/history.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, render_template, session, request
 
 import src.blueprints.utils
-from src.blueprints import db_session, h_api
+from src.blueprints import db_session, h_api, read_config
 from .utils import get_user
+from ..t_to_s import t_to_s, as_origin
 
 history_page = Blueprint("history", __name__)
 
@@ -12,6 +13,8 @@ def history():
     user = get_user(session)
     if not user:
         return render_template("alert.html", message="请登录后使用！")
+    read_c = read_config.get(-1 if not user else user.uid)
+    t_to_s_func = t_to_s if read_c.global_force_simplified else as_origin
     class_ = 1 if not request.args.get("class") else (1 if request.args.get("class") == "article" else 2)
     type_ = 1 if not request.args.get("type") else (1 if request.args.get("type") == "normal" else 0)
     page = 1 if not request.args.get("page") else int(request.args.get("page"))
@@ -26,7 +29,8 @@ def history():
                            history=history_,
                            h_class="article" if class_ == 1 else "series",
                            h_type="normal" if type_ == 1 else "other",
-                           page_info=history_.page_info)
+                           page_info=history_.page_info,
+                           t_to_s=t_to_s_func)
 
 
 @history_page.route("/del_history")
--- src/blueprints/series.py
+++ src/blueprints/series.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, session, render_template
 
-import src.blueprints.utils
 from src.img_replace import img_replace
-from . import s_api, db_session
-from utils import get_user
+from . import s_api, db_session, read_config
+from .utils import get_user
+from ..t_to_s import t_to_s, as_origin
 
 series_page = Blueprint("series", __name__)
 
@@ -13,4 +13,6 @@ def series(sid: int):
     user = get_user(session)
     sid = int(sid)
     series_ = s_api.get_info(sid)
-    return img_replace(render_template("series.html", user=user, series=series_))
+    read_c = read_config.get(-1 if not user else user.uid)
+    t_to_s_func = t_to_s if read_c.global_force_simplified else as_origin
+    return img_replace(render_template("series.html", user=user, series=series_, t_to_s=t_to_s_func))
